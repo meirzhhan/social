4a04d1fee47705425ee756da3e8f487c3acf9192
meirzhhan <mbeymishev@mail.ru>
2023-11-14 23:27:34 +0600

    props, деструтуризация, управляемый компонент, создание UI библиотеки CSS модули

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-  const [value, setValue] = useState('txt in input')
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'}
+  ])
+  const [title, setTitle] = useState('');
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+  }
 
   return (
     <div className="App">
-      <Counter/>
-      <ClassCounter/>
+      <form>
+        {/* Управляемы компонент */}
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"
+        />
+        <MyInput type="text" placeholder="Описание поста"/>
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title={'Посты про JS'}/>
+
     </div>
   );
 }
